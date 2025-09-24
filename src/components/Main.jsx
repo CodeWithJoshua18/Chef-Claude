e2e1dcd349c0a4d8cf8007eebc024b7a0e10d34d
CodeWithJoshua18 <generaljosh5311@gmail.com>
2025-09-24 08:57:19 +0300

    refactor Main component to include ingredient addition form and styles

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -3,7 +3,12 @@ import '../styles/global.css'
 function Main(){
 
     return(
-        <h1>Main component</h1>
+        <main className='add-ingredients'>
+            <form>
+                <input aria-label='Add ingredient' type="text" placeholder='e.g Eggs' />
+                <button>+ Add Ingredient</button>
+            </form>
+        </main>
     )
 }
 
